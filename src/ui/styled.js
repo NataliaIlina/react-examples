b2fa44db41a87736cc7b4c6556aa8363f90fe7bb
Ellianta <metlinova86@gmail.com>
2022-10-25 17:39:12 +0300

    Button component (+fonts)

--- src/ui/styled.js
+++ src/ui/styled.js
@@ -1,6 +1,9 @@
 import styled, { css } from 'styled-components';
 
 export const StyledButton = styled.button`
+  font-family: 'Roboto', Arial, sans-serif;
+  font-size: 14px;
+  line-height: 20px;
   padding: 8px 16px;
   margin: 0;
   outline: none;
@@ -8,12 +11,11 @@ export const StyledButton = styled.button`
   background-color: #1976d2;
   border-radius: 8px;
   display: inline-block;
-  border: 1px solid transparent;
+  border: 1px solid #1976d2;
 
   ${(props) =>
     props.variant === 'outlined' &&
     css`
-      border: 1px solid #1976d2;
       background-color: transparent;
       color: #1976d2;
     `}
