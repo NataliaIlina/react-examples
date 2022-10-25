6092bf3fbde21f680be9f52cea159bb1f89ae07a
Ellianta <metlinova86@gmail.com>
2022-10-25 17:14:35 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyledAppWrapper } from 'src/App.styled';
+
+function App() {
+  return (
+    <StyledAppWrapper>
+      <div>App</div>
+    </StyledAppWrapper>
+  );
+}
+
+export default App;
---
+++ src/App.styled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const StyledAppWrapper = styled.div`
+  max-width: 1200px;
+  min-width: 540px;
+  padding: 24px;
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
