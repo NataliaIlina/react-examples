950ada996327262ae7082174b252ee2ed048c005
Ellianta <metlinova86@gmail.com>
2022-10-25 19:39:21 +0300

    Modal component

---
+++ src/ui/Modal/Modal.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import {
+  StyledModal,
+  StyledModalContent,
+  StyledModalTitle,
+  StyledBackdrop,
+  StyledModalActions,
+  StyledModalHeader,
+  StyledCloseIcon,
+} from 'src/ui/Modal/styled';
+import Button from 'src/ui/Button/Button';
+
+const Modal = ({ isOpen, onClose, title, children, acceptButtonTitle = 'Сохранить', onAccept }) => {
+  const onAcceptButtonClick = () => {
+    if (onAccept) {
+      onAccept();
+    }
+
+    onClose();
+  };
+
+  if (!isOpen) {
+    return null;
+  }
+
+  return (
+    <>
+      <StyledBackdrop onClick={onClose} />
+      <StyledModal onClick={(e) => e.stopPropagation()}>
+        <StyledModalHeader>
+          <StyledModalTitle>{title}</StyledModalTitle>
+          <StyledCloseIcon onClick={onClose}>х</StyledCloseIcon>
+        </StyledModalHeader>
+
+        <StyledModalContent>{children}</StyledModalContent>
+        <StyledModalActions>
+          <Button variant="outlined" onClick={onClose}>
+            Отменить
+          </Button>
+          <Button onClick={onAcceptButtonClick}>{acceptButtonTitle}</Button>
+        </StyledModalActions>
+      </StyledModal>
+    </>
+  );
+};
+
+export default Modal;
