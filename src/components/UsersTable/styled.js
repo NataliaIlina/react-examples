54f9b069bd72f0a712cab84d056d5ef91ca355e3
Ellianta <metlinova86@gmail.com>
2022-10-26 10:49:23 +0300

    Users Table

---
+++ src/components/UsersTable/styled.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+export const StyledTableWrapper = styled.div`
+  background-color: #fff;
+  border-radius: 8px;
+  box-shadow: 0 2px 1px -1px rgb(0 0 0 / 20%), 0px 1px 1px 0px rgb(0 0 0 / 14%),
+    0px 1px 3px 0px rgb(0 0 0 / 12%);
+  width: 100%;
+  overflow-x: auto;
+`;
+
+export const StyledTable = styled.table`
+  display: table;
+  width: 100%;
+  border-collapse: collapse;
+  border-spacing: 0;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export const StyledHeadCell = styled.th`
+  padding: 16px;
+  border-bottom: 1px solid #e7ebf0;
+  text-align: left;
+  font-weight: 600;
+`;
+
+export const StyledCell = styled.td`
+  padding: 16px;
+  border-bottom: 1px solid #e7ebf0;
+  text-align: left;
+`;
