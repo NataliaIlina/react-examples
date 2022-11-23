97efa8b03cf039d6e420e9357b221f59cbb05765
Natalia Ilina <ilinans@msk.bcs.ru>
2022-11-23 18:45:55 +0300

    добавляем типы для картинок

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 
 import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+
+import logo from 'src/img/logo.png';
 
 import Typography from 'ui/Typography/Typography';
 
 const Header = () => (
   <AppBar>
-    <Typography variant="h6" textAlign="center" p="16px">
-      Список пользователей
-    </Typography>
+    <Toolbar>
+      <img src={logo} width={50} height={50} alt="logo" />
+      <Typography variant="h6" textAlign="center" p="16px">
+        Список пользователей
+      </Typography>
+    </Toolbar>
   </AppBar>
 );
 
