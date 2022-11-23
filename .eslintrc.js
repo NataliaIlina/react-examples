cfca458473695eb3e1141c74bfea870e1e717eb5
Natalia Ilina <ilinans@msk.bcs.ru>
2022-11-23 19:30:45 +0300

    17) Мемоизация - настраиваем ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['prettier', 'plugin:prettier/recommended'],
+  extends: ['prettier', 'plugin:prettier/recommended', 'plugin:react-hooks/recommended'],
   plugins: ['@typescript-eslint', 'react', 'prettier', 'unused-imports'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -13,6 +13,12 @@ module.exports = {
     'react/no-array-index-key': 0,
     'react/jsx-props-no-spreading': 0,
     'react/jsx-curly-brace-presence': ['error', 'never'],
+    'react-hooks/exhaustive-deps': [
+      'warn',
+      {
+        enableDangerousAutofixThisMayCauseInfiniteLoops: true,
+      },
+    ],
   },
   overrides: [
     {
