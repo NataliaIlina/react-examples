f4c0fa5f892769d615a041413b704f75a37fba59
Natalia Ilina <ilinans@msk.bcs.ru>
2022-12-13 14:40:53 +0300

    20) Добавляем компонент Select.tsx

--- src/pages/CurrentUser/CurrentUser.tsx
+++ src/pages/CurrentUser/CurrentUser.tsx
@@ -1,11 +1,15 @@
-import React, { useMemo } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 
-import { Stack } from '@mui/material';
+import { SelectChangeEvent, Stack } from '@mui/material';
+import Box from '@mui/material/Box';
 
+import { STATUS_OPTIONS } from 'src/constants/common';
 import { users } from 'src/mock';
+import { IUserStatus } from 'src/types';
 
 import EditableRow from 'ui/EditableRow/EditableRow';
+import Select from 'ui/Select/Select';
 import Typography from 'ui/Typography/Typography';
 
 const CurrentUser = () => {
@@ -14,6 +18,12 @@ const CurrentUser = () => {
   const user = useMemo(() => {
     return users.find((item) => item.id === Number(userId));
   }, [userId]);
+
+  const [status, setStatus] = useState(user?.status);
+
+  const onSelectChange = useCallback((e: SelectChangeEvent) => {
+    setStatus(e.target.value as IUserStatus);
+  }, []);
 
   return (
     <div>
@@ -27,6 +37,16 @@ const CurrentUser = () => {
         <EditableRow label="Фамилия" value={user.lastName} />
         <EditableRow label="Имя" value={user.firstName} />
         <EditableRow label="Почта" value={user.email} />
+
+        <Box width="500px">
+          <Select
+            label="Статус"
+            id="status"
+            options={STATUS_OPTIONS}
+            value={status}
+            onChange={onSelectChange}
+          />
+        </Box>
       </Stack>
     </div>
   );
